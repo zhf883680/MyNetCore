0dd2ca036d292454d24767005c951ab6307bc38d
zhf883680 <zhf883680@gmail.com>
2019-04-16 19:28:53 +0800

    学习npm安装 js引用操作,引用失败处理方法 之类相关操作

--- MyMovie/Startup.cs
+++ MyMovie/Startup.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using MyMovie.Data;
 using MyMovie.Models;
@@ -70,6 +72,10 @@ namespace MyMovie
             }
 
             app.UseStaticFiles();//静态文件  wwwroot 
+            app.UseStaticFiles(new StaticFileOptions { RequestPath = "/node_modules",FileProvider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "node_modules"))});
+
+
+
             app.UseCookiePolicy();
             //app.Run(async (context) =>
             //{
--- MyMovie/study.md
+++ MyMovie/study.md
@@ -35,7 +35,13 @@ _ViewStart.cshtml:  Layout = "~/Views/Shared/_Layout.cshtml"; 开始渲染时候
 _ViewImports:  用于写通用的  例如 @using Models  
 partial view 复用view代码
 view components 复用 独立组件 独立的逻辑/数据 相当于迷你mvc 不依赖父级view数据 需建立 viewComponents components welcome 类似控制器
-
-
+前端  
+新建 npm  使用npm  
+```
+<environment include="Development"></environment>开发环境引用 
+<environment exclude="Development"></environment>非开发环境引用 
+    <script src="~/node_modules/vue/dist/vue.min.js" asp-fallback-src="加载失败 本地地址" asp-fallback-test="Vue"></script> src cdn包
+	css类似 href
+```
 ## 疑问
 ModelOnly 实际展现 目测使用很少?
